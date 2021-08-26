f99a968f24d125f7b00ebde5faef63dd5d4c1abc
Katherine Aguirre <kathesama@gmail.com>
2021-08-26 10:19:56 -0300

    fix: minor fixes

--- mongo/restore.sh
+++ mongo/restore.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+
+# Change --db cleanCode to new database name: --db newDatabaseName.
 mongorestore --db cleanCode ./data/dump-db/cleanCode --drop --authenticationDatabase admin -u root -p pass12345
